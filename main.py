3ba308ebb477f25fc7c7432dc2dc94f2b936b771
mhuber <mhuber@riverbed.com>
2014-10-04 18:07:50 +0200

    again :) git rockz

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from lib.server import GameServer
+from modules.server import GameServer
 import asyncore
 import time
 
@@ -8,16 +8,6 @@ class VT100Codes:
     CLEARSCRN = ESC+"[2J"
     CLEARDOWN = ESC+"[J"
 
-def tick(fps, last_time):
-    interval = 60 / fps
-    current_time = time.time()
-    delta = current_time - last_time
-
-    if delta < interval:
-        time.sleep(interval - delta)
-
-    return time.time()
-
 game_server = GameServer('', 6900)
 
 #NOTE game loop
@@ -26,7 +16,7 @@ while True:
     for addr,handler in game_server.connections.items():
         handler.send_data(VT100Codes.CLEARSCRN)
         handler.send_data(VT100Codes.JMPHOME)
-        handler.send_data("TA-Server / Time: %s, Client: %s\n_____________________________________\nDwarf - Fritz - Fraz" % (time.strftime('%X'), addr[0]))
+        handler.send_data("TA-Server / Time: %s, Client: %s\n\r_____________________________________\n\rDwarf - Fritz - Fraz" % (time.strftime('%X'), addr[0]))
     asyncore.loop(
         timeout=0.1, count=1
     )
