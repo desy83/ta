83abfcf5211000711d551f7f116f79d25c8639c8
basti <sebastian.walsberger@gmx.de>
2014-10-10 14:08:48 +0200

    changed attributes

--- data/models.py
+++ data/models.py
@@ -12,29 +12,36 @@ class Users(BaseModel):
 
 class Classes(BaseModel):
     name = CharField()
-    health = IntegerField()
-    endurence = IntegerField()
-    strength = IntegerField()
-    dexterity = IntegerField
-    wisdom = IntegerField()
-    knowledge = IntegerField()
+    health = IntegerField()    
+    mana = IntegerField()       # for skills
+    strength = IntegerField()   # more dmg meele
+    dexterity = IntegerField()    # more dmg range
+    wisdom = IntegerField()     # more dmg mage
 
 class Races(BaseModel):
     name = CharField()
-    healt = IntegerField()
-    endurence = IntegerField()
+    health = IntegerField()
+    mana = IntegerField()
     strength = IntegerField()
-    dexterity = IntegerField
+    dexterity = IntegerField()
     wisdom = IntegerField()
-    knowledge = IntegerField()
 
 class Char(BaseModel):
     user = ForeignKeyField(Users)
     charclass = ForeignKeyField(Classes)
- 
+    race = ForeignKeyField(Races)
+    level = IntegerField()
+    # absolute values of the char (combined char, race and levelup values)
+    name = CharField()
+    health = IntegerField()
+    mana = IntegerField()
+    strength = IntegerField()
+    dexterity = IntegerField()
+    wisdom = IntegerField()
+
 class Sort(BaseModel):
-    sort = CharField() #healtpot
-    info = TextField()
+    sort = CharField() #healpot
+    info = CharField()
 
 class Items(BaseModel):
     name = CharField()
